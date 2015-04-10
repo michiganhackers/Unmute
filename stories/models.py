523ac67577c471058b029b4e5fa07dd7179946da
Nathan Moos <moosingin3space@gmail.com>
2015-04-09 21:34:09 -0400

    fixed model and added location

--- stories/models.py
+++ stories/models.py
@@ -15,11 +15,11 @@ class Story(db.Model):
     __tablename__ = 'stories'
 
     id = db.Column(db.Integer, primary_key = True)
-        # story's ID
+    # story's ID
 
     # specific post information
-    title = db.Column(db.String(80))
-    story_body = db.Column(db.Text)
+    title = db.Column(db.String(128))
+    story_body = db.Column(db.String(2048))
 
     ColorEnum = create_named_tuple('red', 'pink', 'purple', 'deep_purple', 'indigo', 'blue',
             'light_blue', 'cyan', 'teal', 'green', 'light_green', 'lime', 'yellow', 'amber',
@@ -31,12 +31,14 @@ class Story(db.Model):
     # post time
     post_date = db.Column(ArrowType(), nullable=False)
 
+    # Location
+    latitude = db.Column(db.Float())
+    longitude = db.Column(db.Float())
+
     def __init__(self, title, story_body, color):
         # self.user_id = user_id
         self.title = title
         self.story_body = story_body
         self.color = color
-
         now = arrow.utcnow()
         self.post_date = now.date()
-        self.story_id = story_id
