74d8fdbeb748c2382aad5844ca1dada8706a65ae
Nathan Moos <moosingin3space@gmail.com>
2015-04-13 18:22:30 -0400

    fixed popups

--- static/js/site.js
+++ static/js/site.js
@@ -23,7 +23,7 @@
                     var story = stories.objects[i];
                     if (storiesAdded.indexOf(story.id) < 0) {
                         var marker = L.marker([story.latitude, story.longitude]).addTo(map);
-                        marker.bindPopup("<b>" + story.title + "</b><p>" + story.story_body + "</p>");
+                        marker.bindPopup("<b>" + story.title + "</b><br>" + story.story_body);
                         storiesAdded.push(story.id);
                         numStoriesAdded++;
                     }
